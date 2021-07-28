a7330de8ba4e860bc56e8956422b92d206302c01
loka <hackgaul@gmail.com>
2021-07-28 20:45:07 +0700

    update

--- droper.sh
+++ droper.sh
@@ -17,11 +17,22 @@ rm -rf /var/lib/mysql
 
 echo "\n Remove litespeed ...\n"
 /usr/local/lsws/admin/misc/uninstall.sh
+apt purge -y -qq openlitespeed lsphp74-common lsphp74-curl lsphp74-imap lsphp74-json \
+lsphp74-mysql lsphp74-opcache lsphp74-imagick lsphp74-memcached lsphp74-redis
+
 rm -rf ${LSPATH}
 
+
+
 echo "\n Remove root passprd ...\n"
-/usr/local/lsws/admin/misc/uninstall.sh
 rm -rf ${WWROOT}/*
+
+rm /usr/local/bin/xbackup
+rm /usr/local/bin/xsecure
+rm /usr/local/bin/xdbsave
+rm /usr/local/bin/xdomain
+rm /usr/local/bin/xdrsave
+rm /usr/local/bin/xsecure
 
 apt autoremove
 apt autoclean
