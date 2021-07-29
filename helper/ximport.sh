ddadd8b164c48b0b5c5dd851ffcec400117af16f
loka <hackgaul@gmail.com>
2021-07-30 00:13:30 +0700

    update

--- helper/ximport.sh
+++ helper/ximport.sh
@@ -19,20 +19,20 @@ fi
 SHUSER=$(echo "${DOMAIN}" | sed -e 's/\./-/g')
 DBUSER=$(echo "${DOMAIN}" | sed -e 's/\./-/g')
 DBNAME=$(echo "${DOMAIN}" | sed -e 's/\./_/g')
-FOLDER="/home/${SHUSER}";
+SHROOT="/home/${SHUSER}";
 
-DBPASS=$(cat ${FOLDER}/.datapass | head -n 1 | awk '{print}')
+DBPASS=$(cat ${SHROOT}/.datapass | head -n 1 | awk '{print}')
 if [ "$MYPASS" != '' ]; then
 	DBPASS="${MYPASS}";
 fi
 
 if [ "$ACTION" == "all" ] || [ "$ACTION" == "file" ]; then
 
-	if [ -d "$FOLDER/html" ] && [ -f "${FOLDER}/file.tar.gz" ]; then
-		if [ "$(ls -A $FOLDER/html)" ]; then
+	if [ -d "$SHROOT/html" ] && [ -f "${SHROOT}/file.tar.gz" ]; then
+		if [ ! "$(ls -A $SHROOT/html)" ]; then
 			if [ "$FORCED" == "force" ]; then
 				DOHTML="YES"
-				rm -rf ${FOLDER}/html/*
+				rm -rf ${SHROOT}/html/*
 			fi
 		else
 			DOHTML="YES"
@@ -40,12 +40,12 @@ if [ "$ACTION" == "all" ] || [ "$ACTION" == "file" ]; then
 	fi
 
 	if [ "$DOHTML" == "YES" ]; then
-		tar -xvf ${FOLDER}/file.tar.gz -C ${FOLDER}/html
-		if [ -f "$FOLDER/html/wp-config.php" ]; then
-			sed "/DB_HOST/s/'[^']*'/'${DBPASS}'/2" ${FOLDER}/html/wp-config.php
-			sed "/DB_NAME/s/'[^']*'/'${DBNAME}'/2" ${FOLDER}/html/wp-config.php
-			sed "/DB_USER/s/'[^']*'/'${DBUSER}'/2" ${FOLDER}/html/wp-config.php
-			sed "/DB_PASSWORD/s/'[^']*'/'${DBPASS}'/2" ${FOLDER}/html/wp-config.php
+		tar -xvf ${SHROOT}/file.tar.gz -C ${SHROOT}/html
+		if [ -f "$SHROOT/html/wp-config.php" ]; then
+			sed "/DB_HOST/s/'[^']*'/'${DBPASS}'/2" ${SHROOT}/html/wp-config.php
+			sed "/DB_NAME/s/'[^']*'/'${DBNAME}'/2" ${SHROOT}/html/wp-config.php
+			sed "/DB_USER/s/'[^']*'/'${DBUSER}'/2" ${SHROOT}/html/wp-config.php
+			sed "/DB_PASSWORD/s/'[^']*'/'${DBPASS}'/2" ${SHROOT}/html/wp-config.php
 		fi
 		chown -R ${SHUSER}:${SHUSER} /home/${SHUSER}/html
 	else
@@ -55,20 +55,20 @@ fi
 
 if [ "$ACTION" == "all" ] || [ "$ACTION" == "db" ]; then
 
-	if [ -f "${FOLDER}/data.sql.gz" ]; then
+	if [ -f "${SHROOT}/data.sql.gz" ]; then
 
 		if ! mysql -u root -p${MYPASS} -e "use ${DATABASE};"; then
 			DODATA="YES"
 		elif [ "$FORCED" == "force" ]; then
 			DODATA="YES"
-			mkdir -p ${FOLDER}/data
-			mysqldump -u ${DBUSER} -p${DBPASS} ${DBNAME} | gzip > ${FOLDER}/data/drop-$(date +%Y-%m-%d-%H-%M).sql.gz
+			mkdir -p ${SHROOT}/data
+			mysqldump -u ${DBUSER} -p${DBPASS} ${DBNAME} | gzip > ${SHROOT}/data/drop-$(date +%Y-%m-%d-%H-%M).sql.gz
 			mysql -u ${DBUSER} -p${DBPASS} -e "DROP DATABASE ${DBNAME};"
 		fi
 
 		if [ "$DODATA" == "YES" ]; then
 			mysql -u ${DBUSER} -p${DBPASS} -e "CREATE DATABASE ${DBNAME};"
-			zcat ${FOLDER}/data.sql.gz | mysql -u ${DBUSER} -p${DBPASS} ${DBNAME}
+			zcat ${SHROOT}/data.sql.gz | mysql -u ${DBUSER} -p${DBPASS} ${DBNAME}
 		else
 			echo "database import failed because conditions doesn't satisfy system"
 		fi
