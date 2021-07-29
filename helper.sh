4a3835eeebe9241be4c1241c76ea28a9ecab8ebe
loka <hackgaul@gmail.com>
2021-07-29 13:19:56 +0700

    update

--- helper.sh
+++ helper.sh
@@ -13,46 +13,55 @@ if [ "$ACTION" != 'install' ]; then
 	echo "create command: xdomain"
 	curl -sO ${GITHUB}/master/helper/xdomain.sh
 	chmod +x xdomain.sh
+	rm /usr/local/bin/xdomain
 	mv xdomain.sh /usr/local/bin/xdomain
 	
 	echo "create command: xsavefx"
 	curl -sO ${GITHUB}/master/helper/xsavefx.sh
 	chmod +x xsavefx.sh
+	rm /usr/local/bin/xsavefx;
 	mv xsavefx.sh /usr/local/bin/xsavefx
 
 	echo "create command: xsavedb"
 	curl -sO ${GITHUB}/master/helper/xsavedb.sh
 	chmod +x xsavedb.sh
+	rm /usr/local/bin/xsavedb
 	mv xsavedb.sh /usr/local/bin/xsavedb
 
 	echo "create command: xsavedr"
 	curl -sO ${GITHUB}/master/helper/xsavedr.sh
 	chmod +x xsavedr.sh
+	rm /usr/local/bin/xsavedr
 	mv xsavedr.sh /usr/local/bin/xsavedr
 
 	echo "create command: ximport"
 	curl -sO ${GITHUB}/master/helper/ximport.sh
 	chmod +x ximport.sh
+	rm /usr/local/bin/ximport
 	mv ximport.sh /usr/local/bin/ximport
 	
 	echo "create command: xgetssl"
 	curl -sO ${GITHUB}/master/helper/xgetssl.sh
 	chmod +x xgetssl.sh
+	rm /usr/local/bin/xgetssl
 	mv xgetssl.sh /usr/local/bin/xgetssl
 
 	echo "create command: xgetcms"
 	curl -sO ${GITHUB}/master/helper/xgetcms.sh
 	chmod +x xgetcms.sh
+	rm /usr/local/bin/xgetcms
 	mv xgetcms.sh /usr/local/bin/xgetcms
 
 	echo "create command: xscaner"
 	curl -sO ${GITHUB}/master/helper/xscaner.sh
 	chmod +x xscaner.sh
+	rm /usr/local/bin/xscaner
 	mv xscaner.sh /usr/local/bin/xscaner
 
 	echo "create command: xstatus"
 	curl -sO ${GITHUB}/master/helper/xstatus.sh
 	chmod +x xstatus.sh
+	rm /usr/local/bin/xstatus
 	mv xstatus.sh /usr/local/bin/xstatus
 	
 	exit 1;
