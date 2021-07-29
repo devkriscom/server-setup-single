842921aab421ad2ecb8afadddbabb4cd6a9448d4
loka <hackgaul@gmail.com>
2021-07-30 00:10:03 +0700

    update

--- helper/xrecipe.sh
+++ helper/xrecipe.sh
@@ -26,9 +26,9 @@ if [ -d "${SHHTML}" ] && [ ! "$(ls -A $SHHTML)" ]; then
 		DBMNUM='5.1.1'
 		wget -P ${SHTEMP} https://files.phpmyadmin.net/phpMyAdmin/${DBMNUM}/phpMyAdmin-${DBMNUM}-all-languages.zip
 		unzip ${SHTEMP}/phpMyAdmin-${DBMNUM}-all-languages.zip -d ${SHTEMP}
-		cp -r ${SHHTML}/phpMyAdmin-${DBMNUM}-all-languages/* ${SHHTML}/
+		cp -r ${SHTEMP}/phpMyAdmin-${DBMNUM}-all-languages/* ${SHHTML}/
 		rm -rf ${SHTEMP}/phpMyAdmin-${DBMNUM}-all-languages.zip
-		rm -rf ${SHHTML}/phpMyAdmin-${DBMNUM}-all-languages/*
+		rm -rf ${SHTEMP}/phpMyAdmin-${DBMNUM}-all-languages/*
 		chown -R ${SHUSER}:${SHUSER} ${SHROOT}
 	fi
 else
